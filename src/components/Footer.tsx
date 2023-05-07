aa3713b3f58dab4ca2bc538a5fe503a1640c6cd0
Patryk <patryk-dym@o2.pl>
2023-05-07 15:35:15 +0200

    added solid

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    const today = new Date();
+    return (
+        <footer>
+            <footer>
+                &copy; {today.getFullYear()} YOUR NAME HERE. All rights reserved.
+            </footer>
+        </footer>
+    );
+}
